b089020b08be61a1b1b5002dbfcef350971ebcc4
Siyamul Islam <53119070+siyamulislam@users.noreply.github.com>
2021-12-08 13:46:39 +0600

    array slice splice and join

---
+++ araySlipt.js
@@ -0,0 +1,10 @@
+const num = [1,2,3,4,5,6];
+const part = num.slice(1,1); //slice= (start to endBoundary index)
+console.log(num);
+console.log(part);
+
+const remove = num.splice(1,3,45); //splice = start index  to ,how many to delete, inject elements
+console.log(remove);
+console.log(num);
+const together=num.join(" ~ ")
+console.log(together);
