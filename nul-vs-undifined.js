7eec2d16c50cfc6489a5754fac0a5e26ae0af292
Siyamul Islam <53119070+siyamulislam@users.noreply.github.com>
2021-12-08 09:46:14 +0600

    check undifined

---
+++ nul-vs-undifined.js
@@ -0,0 +1,22 @@
+let value  //value not set
+
+// function add(num1,num2) {
+//     console.log(num1+num2);
+//     return
+// } //invalid return
+// const result =add(14,54)
+// console.log(result);
+
+// function add(num1,num2) {
+//     let sum=num1+num2
+//     return
+// } 
+// const result =add(14)// value not passing
+// console.log(result);
+
+let fun=undefined
+console.log(fun);
+
+let marks=[87,67,99,88]
+console.log(ages[14]);
+
