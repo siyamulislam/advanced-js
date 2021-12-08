05832a4bfd9ad32bb497fa242b9fbad39ea98f9b
Siyamul Islam <53119070+siyamulislam@users.noreply.github.com>
2021-12-08 09:27:44 +0600

    true false added

---
+++ true-false.js
@@ -0,0 +1,21 @@
+// -------------falsy:---------
+// undefined
+// let value
+// let value= false
+// let value= 0
+// let value=""
+// let value= null
+// let value= NaN
+// ------------Truly:----------
+// let value= "0"
+// let value= " "
+// let value=[]
+// let value={}
+
+let value=false
+if(value){
+    console.log("Condition is true");
+}
+else{
+    console.log("Condition is False");
+}
