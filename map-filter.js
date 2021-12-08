e376849490bf34ef06c884e528cf0ed9e32a5756
Siyamul Islam <53119070+siyamulislam@users.noreply.github.com>
2021-12-08 11:38:05 +0600

    use of map and filter to customize code

---
+++ map-filter.js
@@ -0,0 +1,21 @@
+const num = [3,4,5,6,7,8,9]
+//____________way 1________________
+// const out = []
+// for (let i = 0; i < num.length; i++){
+//     const element = num[i]
+//     const result = element*element
+//     out.push(result)
+// }
+// console.log(out);
+//____________way 2________________
+// function square(x) {
+//     return x*x 
+// }
+// const sq = num.map(square)
+// console.log(sq); 
+//____________way 3________________
+const sq = num.map(x=>x*x);
+console.log(sq);
+//____________filter________________
+const broVai = num.filter(x=>x>6);
+console.log(broVai);
