84a85be5de34c9ee4018ede5295131495afa8249
Siyamul Islam <53119070+siyamulislam@users.noreply.github.com>
2021-12-08 10:13:13 +0600

    speacial use of ===

---
+++ double-vs-tripple_equal.js
@@ -0,0 +1,26 @@
+
+const banana=5*2
+const egg="10"
+if(banana==egg){
+    console.log("Price is equal");
+}
+else{
+    console.log("Price is not equal");
+}
+//=== convert both in same type then check
+const egg1=5*2
+const egg2="10"
+if(egg1===egg2){
+    console.log("Price is equal");
+}
+else{
+    console.log("Price is not equal");
+}
+const first=1
+const second=true
+if(first==second){
+    console.log("True");
+}
+else{
+    console.log("False");
+}
