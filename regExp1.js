3dbd49483daa57b9e866cb940fa984fa7a433e5c
siyamulislam <siyamulislam1@gmail.com>
2022-09-12 16:01:33 +0600

    regex  partice carset []1

--- regExp1.js
+++ regExp1.js
@@ -10,9 +10,20 @@ re=/w..ld$/i; //dot match with any character
 re=/he?l?l?o/; // ? mean optional prev char
 re=/he?l?l?o\??/; // escaping ? and check it
 
-str="Hello World";
-str="Hello Wrrld";
-str="hlo!";
+//char Set  []
+re = /h[ea]llo/; // [] must be any one from the bracket
+re = /h[^ia]llo/; // [] anything except from the bracket
+re = /^[hea]ello/; // [] must start any one from the bracket
+re = /[a-z]ello/; // [] any one a to z from the bracket
+re = /^[A-W]/; // First later must be capital ZSiam";
+re = /[A-Za-z]ello/; // any A-Z or a-z0-9A-Z or a-z
+re = /[0-9]ello/; // any 0-9
+re = /[0-9][0-9]ello/; // multiple digit
+re = /^[0-9]ello/; // any must 0-9
+re = /[^0-9]ello/; // any expect 0-9
+
+
+str="hello";
 
 console.log(re.exec(str));
 reTest(re,str);
