6568bca6101f47783cdbfefac132a1ccea4608cd
siyamulislam <siyamulislam1@gmail.com>
2022-09-13 23:15:45 +0600

    data get by ajax

---
+++ ajax.js
@@ -0,0 +1,25 @@
+//AJAX = Asynchronous JavaScript And XML
+document.getElementById('get_data').addEventListener('click',load_data);
+
+function  load_data() { 
+    //create XHE Xml http Request
+    let xhr = new XMLHttpRequest();
+    console.log(xhr);
+    xhr.open('GET','data.txt',true);
+    
+    // xhr.onreadystatechange =function () {
+    //     if(this.status===200 && this.readyState===4){// 200: ok,403:Forbidden; 404 Not Found
+    //         console.log(this.responseText);
+    //         document.getElementById('output').innerHTML=`<h5>${this.responseText}</h5>`;
+    //     }
+    // }
+    xhr.onload =function () {
+        if(this.status===200){// 200: ok,403:Forbidden; 404 Not Found
+            console.log(this.responseText);
+            document.getElementById('output').innerHTML=`<h5>${this.responseText}</h5>`;
+        }
+    }
+    xhr.send();
+    console.log(xhr);
+
+}
