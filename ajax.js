e12932c347f0749b3a5a1f507bcf34d7624e815d
siyamulislam <siyamulislam1@gmail.com>
2022-09-13 23:34:52 +0600

    user delay

--- ajax.js
+++ ajax.js
@@ -6,20 +6,39 @@ function  load_data() {
     let xhr = new XMLHttpRequest();
     console.log(xhr);
     xhr.open('GET','data.txt',true);
-    
+    xhr.onprogress=function () {
+        console.log(xhr.readyState);
+        document.getElementById('output').innerHTML=`<h5>loading...</h5>`;
+   
+    }
+
     // xhr.onreadystatechange =function () {
     //     if(this.status===200 && this.readyState===4){// 200: ok,403:Forbidden; 404 Not Found
     //         console.log(this.responseText);
     //         document.getElementById('output').innerHTML=`<h5>${this.responseText}</h5>`;
     //     }
     // }
+    
+    // xhr.onload =function () {
+    //           if(this.status===200){// 200: ok,403:Forbidden; 404 Not Found
+    //         console.log(this.responseText);
+    //         document.getElementById('output').innerHTML=`<h5>${this.responseText}</h5>`;
+    //     } 
+    
+    // } 
+     
     xhr.onload =function () {
-        if(this.status===200){// 200: ok,403:Forbidden; 404 Not Found
+        setTimeout(() => {
+              if(this.status===200){// 200: ok,403:Forbidden; 404 Not Found
             console.log(this.responseText);
             document.getElementById('output').innerHTML=`<h5>${this.responseText}</h5>`;
-        }
+        } 
+
+    }, 500); //delay .5 sec
+     
     }
     xhr.send();
+  
     console.log(xhr);
 
 }
